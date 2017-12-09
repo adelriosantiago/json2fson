dfb038f5ba850564f13c897addcc93d490a4cbfb
Alejandro del Río <adelriosantiago@gmail.com>
2017-12-08 23:43:01 -0600

    Improve JSON sturucture

--- test.js
+++ test.js
@@ -5,7 +5,26 @@ var json2fson = require('./index.js');
 var data = {
 	'name': 'John Doe',
 	'age': 20,
-	'colors': ['red', 'green', 'blue']
+	'colors': ['red', 'green', 'blue'],
+	'friends': {
+		'goodOnes': [
+			{
+				'NAME': 'Foo',
+				'AGE': 20
+			},
+			{
+				'NAME': 'Bar',
+				'AGE': 30
+			}
+		]
+		'others': [
+			{
+				'NAME': 'FooBar',
+				'AGE': 25
+			}
+		]
+	}
+	'homeAddress': "Main St. #180"
 }
 
 json2fson.convert({ 'data': data });
