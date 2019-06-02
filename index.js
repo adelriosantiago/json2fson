cf3a80a04cce0eac9e8bd51a9f9626f1f8f420a5
Alejandro del Río <adelriosantiago@gmail.com>
2019-06-02 18:04:47 -0500

    Fix `convert` async function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const _ = require('lodash');
 const mkdirp = require('mkdirp');
 const assert = require("chai").assert;
@@ -11,20 +11,31 @@ const assert = require("chai").assert;
 const regex = /[A-Z]/g;
 
 //Default options
+var dbPath = "./fson/"; //The path where the json will be stored as a file
 let defaultOptions;
 
-const convert = (json, options) => {
+let stack = [];
+
+const convert = async (json, options) => {
   assert.isDefined(json);
   
   defaultOptions = _.assign(
     {
-      dbPath: "./fson/", //The path where the json will be stored as a file
       persistent: true, //If not persistent, the dbPath will be deleted and recreated each time removing values that are no longer in the json
+      verbose: false, //If true will explain what is doing
     },
     options
   );
   
-  return _convert(json, undefined);
+  if (defaultOptions.persistent === false) {
+    fs.emptyDirSync(dbPath);
+  }
+  
+  await Promise.all(_convert(json, undefined));
+  
+  if (defaultOptions.verbose) console.log("FSON created");
+  
+  return Promise.resolve(true);
 }
 
 const _convert = (json, subPath) => {
@@ -32,34 +43,51 @@ const _convert = (json, subPath) => {
   
   if (_.isUndefined(subPath)) subPath = "";
   
-  _.forOwn(json, function(v, k) {
-    let slugPath;
+  let tasks = [];
+  
+  tasks = _.map(json, (v, k) => {
     
-    slugPath = path.join(subPath, k).replace(regex, function(match) {
-      
-      return "_" + match.toLowerCase();
-    });
-    
-    let keyPath = path.join(defaultOptions.dbPath, slugPath);
-    
-    if (_.isObject(v)) {
-      //Create folder (will do nothing if the folder already exists)
-      mkdirp(keyPath, function(err) {
-        assert.notExists(err);
-        
-        _convert(v, path.join(subPath, k));
-      });
+    let slugPath = path.join(subPath, k.toString()).replace(regex, function(match) { return "_" + match.toLowerCase(); });
+    let keyPath = path.join(dbPath, slugPath);
+  
+    if (_.isPlainObject(v) || _.isArray(v)) {
+      return new Promise((res, rej) => {
+        fs.ensureDir(keyPath, async (err) => {
+          assert.notExists(err);
+          
+          if (defaultOptions.verbose) console.log("Folder created:", keyPath);
+          await Promise.all(_convert(v, path.join(subPath, k.toString())));
+          
+          return res();
+        });
+      })
     } else {
-      //Create file (will overwrite contents)
-      fs.writeFile(keyPath, v, function(err) {
-        assert.notExists(err);
+      return new Promise((res, rej) => {
+        fs.writeFile(keyPath, v, function(err) {
+          assert.notExists(err);
+          
+          if (defaultOptions.verbose) console.log("File created:", keyPath);
+          
+          return res();
+        });
       });
     }
   });
   
-  return true;
+  return tasks;
 }
 
-module.exports = {
-  convert
-};
+const changeDbPath = (newPath) => {
+  if (_.isString(newPath)) {
+    try {
+      fs.ensureDirSync(newPath);
+    } catch (e) {
+      throw e;
+    } finally {
+      dbPath = newPath;
+      return true;
+    }
+  }
+}
+
+module.exports = { convert, changeDbPath };
